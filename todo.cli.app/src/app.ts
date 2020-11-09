298752b7b62635ebf22c393d436e80cfc953a480
Oleh Bondaruk <bondaruk9000@gmail.com>
2020-11-10 00:28:18 +0100

    added inquirer package

--- todo.cli.app/src/app.ts
+++ todo.cli.app/src/app.ts
@@ -1,11 +1,35 @@
 import {ItemCollection} from './items/ItemCollection';
+import * as inquirer from 'inquirer'
 
-const collectionName = 'Work';
-const itemTask = 'Learn TS';
-const id = 1;
+enum Commands {
+  Quit = 'Quit'
+}
 
-const itemCollection = new ItemCollection(collectionName);
-itemCollection.addItem(itemTask, id);
-itemCollection.completeTask(id);
+function displayToDoList(list: ItemCollection): void {
+  console.log(`${list.name} todo list, ${list.getCount()} items todo.`);
+  list.getItems().forEach(item => item.printDetails())
+}
+
+function promptUser(collection: ItemCollection): void {
+  const promptConf = {type: 'list', name: 'command', message: 'Choose option', choices: Object.values(Commands)};
+
+  console.clear();
+  displayToDoList(collection);
+  // @ts-ignore
+  inquirer.prompt(promptConf).then(answers => {
+    if (answers['command'] !== Commands.Quit) {
+      promptUser(collection);
+    }
+  });
+}
+
+
+const itemCollection = new ItemCollection('John Work');
+itemCollection.addItem('Learn TS', 1);
+itemCollection.addItem('Create project', 2);
+itemCollection.completeTask(1);
 // itemCollection.removeCompleted();
-console.log(itemCollection.getCount())
+// console.log(itemCollection.getCount())
+
+promptUser(itemCollection);
+
